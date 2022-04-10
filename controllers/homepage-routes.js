1d39b4c2462220260282c13482a7dbdacce886aa
alen <alen.dunn1@gmail.com>
2022-04-09 21:32:28 -0400

    add single podcast endpoint and base template

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,3 +1,5 @@
+const { Podcast, Comment, User, Genre } = require("../models");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -10,4 +12,40 @@ router.get("/", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
+// podcast page
+router.get("/podcasts/:id", (req, res) => {
+  Podcast.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Genre,
+      },
+      {
+        model: Comment,
+        attributes: ["id", "title", "comment_text", "createdAt"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((podData) => {
+      const podcast = podData.get({ plain: true });
+      console.log(podcast);
+      res.render("podcast", podData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
+
+// ('/comedy' req, res => {
+//podcast finall
+//})
