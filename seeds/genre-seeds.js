e3f302cd7a69bc2a42f30d50d9a6428eeed47ac8
alen <alen.dunn1@gmail.com>
2022-04-12 13:59:49 -0400

    merge

--- seeds/genre-seeds.js
+++ seeds/genre-seeds.js
@@ -14,15 +14,11 @@ const genreData = [
     genre_name: "Technology",
   },
   {
-<<<<<<< HEAD
-    genre_name: "Documentary",
-=======
-    genre_name: 'Educational',
->>>>>>> develop
+    genre_name: "Educational",
   },
   {
-    genre_name: 'Entertainment',
-  }
+    genre_name: "Entertainment",
+  },
 ];
 
 const seedGenres = () => Genre.bulkCreate(genreData);
